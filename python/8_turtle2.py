ff6a59881221d0fbba230d7a897c1f00a64b1940
Robert <robert.szczepanski@cmsrs.pl>
2025-01-23 16:35:16 +0100

    circle

--- python/8_turtle2.py
+++ python/8_turtle2.py
@@ -8,11 +8,17 @@ t = turtle.Turtle()
 t.speed(5) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
 
 for i in range(90, 0, -10):    
-    c1 = random.random()
-    c2 = random.random()
-    c3 = random.random()
-    t.begin_fill()
+    #cli
+    #c1 = random.random()
+    #c2 = random.random()
+    #c3 = random.random()
+
+    #online
+    c1 = random.randint(0,255)
+    c2 = random.randint(0,255)
+    c3 = random.randint(0,255)
     t.fillcolor(c1, c2, c3)
+    t.begin_fill()    
     t.circle(i)
     t.end_fill()
 
