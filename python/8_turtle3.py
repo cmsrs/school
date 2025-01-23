9fa9fab0fdfe563924b93403369554bb7952149c
Robert <robert.szczepanski@cmsrs.pl>
2025-01-23 16:49:15 +0100

    4 trinagles

---
+++ python/8_turtle3.py
@@ -0,0 +1,24 @@
+# https://pythonsandbox.com/turtle
+# https://www.migra.pl/programowanie/python-turtle
+
+
+import turtle
+import math
+
+t = turtle.Turtle()
+t.speed(5) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
+
+
+def draw_triangle(t):
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90+45)
+    t.forward(100 * math.sqrt(2) )
+
+for i in range(4):    
+    draw_triangle(t)
+    t.right(45 + 90)
+
+
+turtle.done()
