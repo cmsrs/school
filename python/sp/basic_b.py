819e5655c41a39c9464b18ea65606b709d499997
Robert <robert.szczepanski@cmsrs.pl>
2025-02-07 17:34:21 +0100

    basic b

--- python/sp/basic_b.py
+++ python/sp/basic_b.py
@@ -14,16 +14,37 @@ print('######2######')
 j = 10
 for _ in range(10):
     print(j)    
-    j = j + 1
+    j = j + 2
 
 print('######3######')
 
 j = 10
 for i in range(10):
-    print(j)    
+    print("index", i, " j:",  j)    
     j = j + i
 
 print('######4######')
 
 for i in range(10, 100, 10):
     print(i)
+
+print('######5######')
+
+list5 = [10, 20, 30]
+
+for i in range(3):
+    print("index: ", i, " value: ", list5[i])
+
+print('######6######')
+
+list6 = ['red', 'green', 'blue']
+
+for i in range(3):
+    print("index: ", i, " value: ", list6[i])
+
+print('######7######')
+
+list7 = ['red', 'green', 'blue']
+
+for i in list7:
+    print("index: ", i)
