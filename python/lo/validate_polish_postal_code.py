4083ca2b25bb33c3c749adab8aa7504a87e1e1cc
Robert <robert.szczepanski@cmsrs.pl>
2025-02-09 12:57:21 +0100

    validate polish postal code

---
+++ python/lo/validate_polish_postal_code.py
@@ -0,0 +1,26 @@
+"""
+
+Funkcja weryfikuje, czy podany ciąg znaków spełnia format polskiego kodu pocztowego (NN-NNN), gdzie N oznacza cyfrę. 
+Sprawdza długość ciągu, obecność myślnika na trzeciej pozycji oraz to, czy pozostałe znaki są cyframi.
+
+"""
+def validate_polish_postal_code(postal_code):
+    # Sprawdzenie długości
+    if len(postal_code) != 6:
+        return False
+    
+    # Sprawdzenie formatu XX-XXX
+    if not (postal_code[0].isdigit() and postal_code[1].isdigit()):
+        return False
+    if postal_code[2] != "-":
+        return False
+    if not (postal_code[3].isdigit() and postal_code[4].isdigit() and postal_code[5].isdigit()):
+        return False
+
+    return True
+
+
+assert True == validate_polish_postal_code("01-234")
+assert False == validate_polish_postal_code("123-45")
+assert False == validate_polish_postal_code("AB-123")
+assert True == validate_polish_postal_code("12-345")
