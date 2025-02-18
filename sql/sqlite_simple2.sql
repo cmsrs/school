97419682604c4116663dbbe057bb8234bcee873b
Robert <robert.szczepanski@cmsrs.pl>
2025-02-18 19:02:13 +0100

    simple 2

--- sql/sqlite_simple2.sql
+++ sql/sqlite_simple2.sql
@@ -14,6 +14,22 @@ CREATE TABLE students (
 );
 
 
+-- Dodanie przykładowych danych do tabeli classes
+INSERT INTO classes (name) VALUES 
+('7a'), 
+('5b');
+
+-- Dodanie przykladowych danych do tabeli students
+INSERT INTO students (first_name, last_name, class_id) 
+VALUES 
+('John', 'Smith', 1), 
+('Anna', 'Brown', 1), 
+('Peter', 'Johnson', 2), 
+('Mary', 'Williams', 2),
+('John', 'Doe', NULL), 
+('Kristen', 'Bell', 2);
+
+
 -- wybierz wszystkich studentow
 select * from students;
 
