cfdc83ea14c54180ce742b13cfd1d1b77d2538b7
Robert <robert.szczepanski@cmsrs.pl>
2026-01-10 19:50:50 +0100

    excel zadania

---
+++ excel/collective/print_zestawy.py
@@ -0,0 +1,14 @@
+# folder, użytkownik i repo
+user = "cmsrs"
+repo = "school"
+branch = "main"
+path_in_repo = "excel/collective/zestawy_dla_uczniow"
+
+print("### 📥 Pobierz swój zestaw danych\n")
+
+for i in range(1, 51):  # od 01 do 35
+    num = f"{i:03}"  #  numer: 000, 001, 002 ...
+    file_name = f"zestaw_{num}.xlsx"
+    url = f"https://raw.githubusercontent.com/{user}/{repo}/{branch}/{path_in_repo}/{file_name}"
+    print(f"- Zestaw {num} → [Pobierz {file_name}]({url})")
+
