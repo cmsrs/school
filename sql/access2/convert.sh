d146f19501eb3f869d3e14df0675d83c415d547a
Robert <robert.szczepanski@cmsrs.pl>
2026-01-29 19:37:03 +0100

    znak

---
+++ sql/access2/convert.sh
@@ -0,0 +1,6 @@
+convert -density 300 tmp.pdf \
+-fill "rgba(0,0,0,0.25)" \
+-gravity center \
+-pointsize 48 \
+-annotate 45x0+0-200 "Materiał edukacyjny – rs" \
+liczba_ocen_ucznia_robert_szczepanski_4lb_nr30.pdf 
