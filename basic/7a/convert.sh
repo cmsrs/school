676704010ecaa4e5c90af4e66b077cfdc6f6b237
Robert <robert.szczepanski@cmsrs.pl>
2026-02-09 20:06:53 +0100

    znak wodny

---
+++ basic/7a/convert.sh
@@ -0,0 +1,6 @@
+convert -density 300  7h_robert_szczepanski_org.pdf \
+-fill "rgba(0,0,0,0.25)" \
+-gravity center \
+-pointsize 48 \
+-annotate 45x0+0-200 "Materiał edukacyjny – rs" \
+7h_robert_szczepanski.pdf
